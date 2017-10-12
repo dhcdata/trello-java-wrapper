e6f748ce9bcd58a449601bb6d682938f5f12e30a
Chris Howe <cjhowe@dhcdata.com>
2017-10-12 13:10:26 -0500

    support labels with name

--- src/main/java/com/julienvey/trello/domain/Label.java
+++ src/main/java/com/julienvey/trello/domain/Label.java
@@ -8,6 +8,15 @@ public class Label extends TrelloEntity {
     private String color;
     private String name;
 
+    public Label() {
+    }
+
+    public Label(String value) {
+      String[] parts = value.split(",", 2);
+      color = parts[0].trim();
+      if(parts.length>1) name = parts[1].trim();
+    }
+
     /* Accessors */
     public String getColor() {
         return color;
